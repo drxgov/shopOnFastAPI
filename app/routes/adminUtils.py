f0ef294a83eb9ecf9965ce4f83218f2a98758f33
drxgov <retriveserenity@gmail.com>
2025-10-15 15:37:13 +0300

    чота добавление товаров сделал

---
+++ app/routes/adminUtils.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter,Request
+from fastapi.templating import Jinja2Templates
+
+router = APIRouter()
+templates = Jinja2Templates(directory="templates")
+
+@router.get('/additem')
+async def addItem(request: Request):
+    userlogin = request.cookies.get('user_login')
+    return templates.TemplateResponse('addItem.html',{
+        'request':request,
+        'userlogin':userlogin
+    })
